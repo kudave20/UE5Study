bd8ea259493a261d9e92369f6ac752cd5621e6f0
DY <dave1315@korea.ac.kr>
2023-05-01 22:43:00 +0900

    문 블루프린트 클래스 생성

---
+++ Source/UE5Study/Prop/Door.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GeometryCollection/GeometryCollectionActor.h"
+#include "Door.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5STUDY_API ADoor : public AGeometryCollectionActor
+{
+	GENERATED_BODY()
+
+};
